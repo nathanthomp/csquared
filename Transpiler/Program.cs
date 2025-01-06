760b42ed6093b29a9d587283f18fa5b0dbcc70b5
Nathan Thompson <thompn18@email.franklin.edu>
2025-01-06 16:50:53 -0500

    Restart in .NET

---
+++ Transpiler/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Transpiler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+
+            Console.ReadLine();
+        }
+    }
+}
